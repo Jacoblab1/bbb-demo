acc3e3c1eb78925cd2f1d1ba2f987700fc9dc36f
Jacoblab1 <jacoblab1tube@gmail.com>
2019-01-10 17:21:50 -0500

    Missing fields warning added for meeting creation

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -2,8 +2,13 @@ require 'bigbluebutton_api'
 
 class MeetingsController < ApplicationController
   def create
-    create_meeting(meeting_params[:name], meeting_params[:id], meeting_params[:modPW], meeting_params[:attPW], meeting_params[:recording]) # create on bbb server
-    redirect_to meetings_path
+    if meeting_params[:name] == "" || meeting_params[:id] == "" || meeting_params[:modPW] == "" || meeting_params[:attPW] == ""
+      flash[:missing] = true
+      redirect_to new_meeting_path
+    else
+      create_meeting(meeting_params[:name], meeting_params[:id], meeting_params[:modPW], meeting_params[:attPW], meeting_params[:recording]) # create on bbb server
+      redirect_to meetings_path
+    end
   end
 
   private def meeting_params
