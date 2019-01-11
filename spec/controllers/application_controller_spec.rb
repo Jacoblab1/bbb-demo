79f7396c27fc99de0506ac33239fa02297fc1f9e
Jacoblab1 <jacoblab1tube@gmail.com>
2019-01-11 15:20:53 -0500

    Added int_to_boolean rspec test

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe ApplicationController do
       controller.get_meetings[:returncode].should == true
     end
   end
+
+  describe "int_to_boolean" do
+    it "returns true if given integer value 1" do
+      controller.int_to_boolean(1).should == true
+    end
+
+    it "returns false if given anything not 1" do
+      controller.int_to_boolean("abc").should == false
+    end
+  end
 end
