8405e0dca31762070365c31367b2d85ee056971f
Jacoblab1 <jacoblab1tube@gmail.com>
2019-01-11 13:57:03 -0500

    Moved duplicate code to a before block

--- spec/controllers/connect_controller_spec.rb
+++ spec/controllers/connect_controller_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe ConnectController do
+  before do
+    controller.params[:password] = ""
+    controller.params[:username] = "Guest"
+    controller.params[:id] = "123"
+  end
+  
   describe "check_valid" do
     it "checks for empty input" do
-      controller.params[:password] = ""
-      controller.params[:username] = "Guest"
-      controller.params[:id] = "123"
       controller.check_valid.should == false
     end
   end
 
   describe "join_meeting_url" do
     it "checks that a url is returned" do
-      controller.params[:password] = ""
-      controller.params[:username] = "Guest"
-      controller.params[:id] = "123"
       controller.get_meeting_url.should_not == nil
     end
   end
